d2ed093175c3751a4ef1820b3ca9182da61d6aa4
aakash-prectech <aakash@prectech.online>
2026-01-10 19:17:21 +0530

    publisher id added to meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -169,6 +169,11 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Emoji Keyboard" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4360932072488893"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body className={`font-sans antialiased`}>
         <ThemeProvider attribute="class">
